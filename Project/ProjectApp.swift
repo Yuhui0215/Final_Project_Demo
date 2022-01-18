09fe38e7b52b23a6b0edba37c2143cfda6b8deed
Yuhui <00857104@emai.ntou.edu.tw>
2022-01-18 20:16:50 +0800

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Len on 2022/1/18.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
